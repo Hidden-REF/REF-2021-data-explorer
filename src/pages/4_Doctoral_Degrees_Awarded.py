8224a37133cbb2270f22d71e38664426f74c5506
mihaeladuta <mihaela.duta@psy.ox.ac.uk>
2023-08-29 20:31:32 +0100

    Added histogram to degrees.

--- src/pages/4_Doctoral_Degrees_Awarded.py
+++ src/pages/4_Doctoral_Degrees_Awarded.py
@@ -2,6 +2,7 @@ import pandas as pd
 import streamlit as st
 from streamlit_extras.dataframe_explorer import dataframe_explorer
 from streamlit_extras.chart_container import chart_container
+import plotly.figure_factory as ff
 
 import codebook as cb
 import read_write as rw
@@ -44,6 +45,20 @@ for i, column in enumerate(columns):
         with chart_container(dset_stats):
             vis.draw_counts_percent_chart(dset_stats,
                                           column)
+
+st.subheader(cb.COL_DEGREES_TOTAL)
+bin_size = st.slider("Select the bin size",
+                     min_value=1,
+                     max_value=50,
+                     value=10,
+                     step=1)
+
+fig = ff.create_distplot([dset[cb.COL_DEGREES_TOTAL]],
+                         [cb.COL_DEGREES_TOTAL],
+                         histnorm="probability",
+                         bin_size=bin_size)
+fig.update_layout(showlegend=False)
+st.plotly_chart(fig, use_container_width=True)
 
 st.subheader(sh.EXPLORE_HEADER)
 dset_explore = dataframe_explorer(dset)
