7cd2950c06d4db50b9a7de65a5e79e4a083b92b0
mihaeladuta <mihaela.duta@psy.ox.ac.uk>
2023-08-30 12:48:46 +0100

    Fixed home page display.

--- src/Home.py
+++ src/Home.py
@@ -19,15 +19,14 @@ st.header("Data source")
 st.markdown(sh.DATA_INFO)
 
 st.header("About the REF 2021 process")
-text_to_display = \
-    """Submissions for REF 2021 consisted of
-    - Outputs (e.g. journal articles, books, datasets, etc.)
-    - Impact Case Studies
-    - Research Groups
-    - Research Income and Research Income in Kind
-    - Research Environment statements for each institution and unit
-    - Doctoral Degrees Awarded
-    """
+text_to_display = """Submissions for REF 2021 consisted of
+- Outputs (e.g. journal articles, books, datasets, etc.)
+- Impact Case Studies
+- Research Groups
+- Research Income and Research Income in Kind
+- Research Environment statements for each institution and unit
+- Doctoral Degrees Awarded
+"""
 st.markdown(text_to_display)
 
 
