644d08d244ebc09a04c94c24729cc64f6effd8ab
mihaeladuta <mihaela.duta@psy.ox.ac.uk>
2023-08-30 16:33:00 +0100

    Update the impact page.

--- src/pages/3_Impact_Case_Studies.py
+++ src/pages/3_Impact_Case_Studies.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import streamlit as st
 from streamlit_extras.dataframe_explorer import dataframe_explorer
 from streamlit_extras.chart_container import chart_container
@@ -9,49 +8,42 @@ import process as proc
 import visualisations as vis
 import shared as sh
 
+STRING_COLUMNS = [cb.COL_IMPACT_TITLE,
+                  cb.COL_IMPACT_COUNTRIES,
+                  cb.COL_IMPACT_FORMAL_PARTNERS,
+                  cb.COL_IMPACT_FUNDING_PROGS,
+                  cb.COL_IMPACT_GLOBAL_RID,
+                  cb.COL_IMPACT_FUNDERS_NAME,
+                  cb.COL_IMPACT_ORCIDs,
+                  cb.COL_IMPACT_GRANT_FUNDING,
+                  cb.COL_IMPACT_SUMMARY,
+                  cb.COL_IMPACT_UNDERPIN_RESEARCH,
+                  cb.COL_IMPACT_REFERENCES_RESEARCH,
+                  cb.COL_IMPACT_DETAILS,
+                  cb.COL_IMPACT_CORROBORATE]
+CATEGORIES_COLUMNS = [cb.COL_IMPACT_CONTINUED]
 
 page_title = "Impact Case Studies"
 st.title(page_title)
 
 with st.spinner(sh.PROC_TEXT):
     (dset, _) = rw.get_data(rw.DATA_PPROC_IMPACTS,
-                            categories_columns=[cb.COL_IMPACT_CONTINUED],
-                            string_columns=[cb.COL_IMPACT_TITLE,
-                                            cb.COL_IMPACT_COUNTRIES,
-                                            cb.COL_IMPACT_FORMAL_PARTNERS,
-                                            cb.COL_IMPACT_FUNDING_PROGS,
-                                            cb.COL_IMPACT_GLOBAL_RID,
-                                            cb.COL_IMPACT_FUNDERS_NAME,
-                                            cb.COL_IMPACT_ORCIDs,
-                                            cb.COL_IMPACT_GRANT_FUNDING,
-                                            cb.COL_IMPACT_SUMMARY,
-                                            cb.COL_IMPACT_UNDERPIN_RESEARCH,
-                                            cb.COL_IMPACT_REFERENCES_RESEARCH,
-                                            cb.COL_IMPACT_DETAILS,
-                                            cb.COL_IMPACT_CORROBORATE]
-                            )
+                            categories_columns=CATEGORIES_COLUMNS,
+                            string_columns=STRING_COLUMNS)
+print(dset.columns)
 
-dset_to_print = pd.DataFrame.from_dict(
-    {
-        "Impact case studies records": dset.shape[0],
-        "Institutions": dset[cb.COL_INST_NAME].nunique()
-    },
-    orient="index"
-    )
-dset_to_print.columns = ["count"]
-pd.set_option("display.max_colwidth", None)
-st.dataframe(dset_to_print)
+vis.display_record_counts_table(dset)
 
+# distribution charts
+# -------------------
 st.subheader(sh.CHARTS_HEADER)
-st.markdown("Select from the charts below to view the "
-            "distributions for the number of impact case studies records by ...")
+dcolumns = [cb.COL_PANEL_NAME,
+            cb.COL_UOA_NAME,
+            cb.COL_IMPACT_CONTINUED]
+dtabs = st.tabs(dcolumns)
 
-columns = [cb.COL_PANEL_NAME,
-           cb.COL_UOA_NAME]
-tabs = st.tabs(columns)
-
-for i, column in enumerate(columns):
-    with tabs[i]:
+for i, column in enumerate(dcolumns):
+    with dtabs[i]:
         dset_stats = proc.calculate_counts(dset,
                                            column,
                                            sort=False)
@@ -59,6 +51,68 @@ for i, column in enumerate(columns):
             vis.draw_counts_percent_chart(dset_stats,
                                           column)
 
+# select categorical values and export
+# ------------------------------------
+st.divider()
+st.markdown(f"## {sh.SELECT_EXPORT_HEADER_PREFIX} ...")
+
+scolumns = dcolumns.copy()
+stabs = st.tabs(scolumns)
+
+for i, column in enumerate(scolumns):
+    with stabs[i]:
+        options = list(dset[column].cat.categories)
+        prompt = f"##### Select `{column}` value"
+        option = st.selectbox(prompt,
+                              [""] + options,
+                              index=0,
+                              help="Select a value to filter the data")
+
+        if option:
+            with st.spinner(sh.PROC_TEXT):
+                dset_selected = dset[dset[column] == option]
+            dict = {"Selected": option,
+                    "Records": dset_selected.shape[0]}
+            vis.display_table_from_dictionary(dict)
+            rw.download_data(dset_selected,
+                             sh.DOWNLOAD_SELECTED_DATA_PROMPT,
+                             "selected_data.csv")
+
+# search string fields for pattern
+# --------------------------------
+st.divider()
+st.markdown(f"## {sh.SEARCH_EXPORT_HEADER_PREFIX} ...")
+pcolumns = [cb.COL_IMPACT_TITLE,
+            cb.COL_IMPACT_SUMMARY,
+            cb.COL_IMPACT_UNDERPIN_RESEARCH,
+            cb.COL_IMPACT_REFERENCES_RESEARCH,
+            cb.COL_IMPACT_DETAILS]
+
+ptabs = st.tabs(pcolumns)
+
+for i, column in enumerate(pcolumns):
+    with ptabs[i]:
+        prompt = f"Enter a pattern to search '{column}'"
+        pattern = st.text_input(prompt,
+                                max_chars=40,
+                                value="",
+                                help="Enter a pattern to search for"
+                                )
+        if pattern:
+            with st.spinner(sh.PROC_TEXT):
+                dset_selected = dset[dset[column].str.contains(pattern,
+                                                               case=False,
+                                                               na=False)]
+            dict = {"Pattern": pattern,
+                    "Records": dset_selected.shape[0]}
+            vis.display_table_from_dictionary(dict)
+            rw.download_data(dset_selected,
+                             sh.DOWNLOAD_SELECTED_DATA_PROMPT,
+                             "selected_data.csv")
+
+# explore data
+# ------------
+st.divider()
 st.subheader(sh.EXPLORE_HEADER)
 dset_explore = dataframe_explorer(dset)
 st.dataframe(dset_explore, use_container_width=False)
