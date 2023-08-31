114b52995125ad70c94ec3abce2c3ade692b1791
mihaeladuta <mihaela.duta@psy.ox.ac.uk>
2023-08-31 13:45:48 +0100

    Do not plot distributions for the number of records for income source records because they don't mean anything without the attached sum.

--- src/codebook.py
+++ src/codebook.py
@@ -96,7 +96,8 @@ COL_RESULTS_UNCLASSIFIED_BINNED = f"{COL_RESULTS_UNCLASSIFIED} (binned)"
 
 CATEGORY_FIELDS_EXCLUDE_CHARTS = [COL_INST_CODE,
                                   COL_INST_NAME,
-                                  COL_RG_CODE]
+                                  COL_RG_CODE,
+                                  COL_INCOME_SOURCE]
 
 # value to add if no entry in the data
 VALUE_ADDED_NOT_SPECIFIED = 'Not specified'
