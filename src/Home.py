18d8989dc973d71ea6ed111359c0d0f646282e81
Mihaela Duta <mihaela.duta@dtc.ox.ac.uk>
2023-11-24 16:07:48 +0000

    Simplified the main page.

--- src/Home.py
+++ src/Home.py
@@ -19,33 +19,15 @@ st.header("Data source")
 st.markdown(sh.DATA_INFO)
 
 st.header("About the REF 2021 process")
-text_to_display = """Submissions for REF 2021 consisted of
+text_to_display = """Submissions for REF 2021 consisted of information on
+- Research Groups
 - Outputs (e.g. journal articles, books, datasets, etc.)
 - Impact Case Studies
-- Research Groups
-- Research Income and Research Income in Kind
-- Research Environment statements for each institution and unit
 - Doctoral Degrees Awarded
+- Research Income
+- Research Income in Kind
+- Research Environment 
+
+Use the sidebar to navigate between the data sets to visualise and explore the data.
 """
 st.markdown(text_to_display)
-
-
-columns = [cb.COL_PANEL_NAME,
-           cb.COL_UOA_NAME,
-           cb.COL_OUTPUT_TYPE_NAME,
-           cb.COL_OPEN_ACCESS,
-           cb.COL_INCOME_SOURCE,
-           cb.COL_INCOMEINKIND_SOURCE
-           ]
-
-tabs = st.tabs(columns)
-
-for itab, tab in enumerate(tabs):
-    with tab:
-        column = columns[itab]
-        dict = cb.enum_values[column]
-        dset_to_print = pd.DataFrame.from_dict(dict,
-                                               orient="index",
-                                               columns=[column])
-        dset_to_print.index.name = "Code"
-        st.dataframe(dset_to_print)
