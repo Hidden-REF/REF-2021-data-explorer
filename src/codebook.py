0c5f6b2138bc33ee8001257457b41ea72ccc7498
Mihaela Duta <mihaela.duta@dtc.ox.ac.uk>
2024-01-26 12:26:18 +0000

    Updated codebook.

--- src/codebook.py
+++ src/codebook.py
@@ -98,6 +98,7 @@ COL_RESULTS_UNCLASSIFIED_BINNED = f"{COL_RESULTS_UNCLASSIFIED} (binned)"
 COL_RESULTS_PROFILE_VALUES = ["Environment", "Outputs", "Impact", "Overall"]
 COL_RESULTS_PROFILE_PREFIXES = [f"{column} evaluation -" for column in COL_RESULTS_PROFILE_VALUES] 
 COL_RESULTS_RG_SUBMISSIONS = 'Research group submissions'
+COL_RESULTS_OUTPUT_SUBMISSIONS = 'Output submissions'
 
 CATEGORY_FIELDS_EXCLUDE_CHARTS = [COL_INST_CODE,
                                   # COL_RG_CODE,
