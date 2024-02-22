ba89b524882866577fabaa250a37588338e59d6f
Mihaela Duta <mihaela.duta@dtc.ox.ac.uk>
2024-02-22 14:33:33 +0000

    Cleaned code.

--- src/REF2021_explorer/pages/8_Results.py
+++ src/REF2021_explorer/pages/8_Results.py
@@ -20,18 +20,3 @@ with st.expander(sh.DESCRIBE_HEADER):
 
 with st.expander(sh.LOGS_HEADER):
     vis.display_logs(logs)
-
-# vis.display_record_counts_table(dset, logs, description=sh.RESULTS_DESCRIPTION)
-# with st.expander(sh.VISUALISE_HEADER):
-#     tabs = st.tabs(
-#         [
-#             sh.DISTRIBUTIONS_TAB_HEADER,
-#             sh.HISTOGRAMS_TAB_HEADER,
-#         ]
-#     )
-#     with tabs[0]:
-#         vis.display_distributions(dset)
-#     with tabs[1]:
-#         vis.display_histograms(dset)
-# with st.expander(sh.EXPLORE_HEADER):
-#     vis.display_data_explorer(dset, do_histograms=True)
